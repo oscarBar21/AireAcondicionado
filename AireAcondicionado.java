5320cf3bdf38ac8545312d6e74c9cd6a7e9ae3ee
oscar <oscarbarragan31@gmail.com>
2021-01-25 09:41:25 +0000

    0497.02 añadi el constructor de la clase y dos metodos para variar la temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,33 +1,21 @@
-
-/**
- * Write a description of class AireAcondicionado here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class AireAcondicionado
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private double temperatura;
 
-    /**
-     * Constructor for objects of class AireAcondicionado
-     */
     public AireAcondicionado()
     {
-        // initialise instance variables
-        x = 0;
+        temperatura = 15.0 ;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void subirTemperatura(){
+        temperatura = temperatura + 5;
+    }
+    
+    public void bajarTemperatura(){
+        temperatura = temperatura - 5;
+    }
+    
+    public double getTemperatura(){
+        return temperatura ;
     }
 }
