21be1edccb275be854bdda728918a828c65dcb5b
oscar <oscarbarragan31@gmail.com>
2021-01-26 09:19:26 +0000

    0497.03 modifique los metodos subir y bajar temperatura  y el metodo incrementar

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,21 +1,37 @@
 public class AireAcondicionado
 {
     private double temperatura;
+    private double min;
+    private double max;
+    private double incremento;
 
-    public AireAcondicionado()
+    public AireAcondicionado(double temperaturaMin, double temperaturaMax)
     {
         temperatura = 15.0 ;
+        min = temperaturaMin;
+        max = temperaturaMax;
+        incremento = 5.0;
     }
-    
+  
     public void subirTemperatura(){
-        temperatura = temperatura + 5;
+        if( temperatura + incremento <= max){
+            temperatura = temperatura + incremento;
+        }
     }
-    
+ 
     public void bajarTemperatura(){
-        temperatura = temperatura - 5;
+        if(temperatura - incremento >= min){
+            temperatura = temperatura - incremento;
+        }
     }
     
     public double getTemperatura(){
         return temperatura ;
+    }
+    
+    public void setIncremento(double incremento){
+        if (incremento >= 0){
+            this.incremento = incremento;
+        }
     }
 }
