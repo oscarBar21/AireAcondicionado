f421f727e19a5bd47da641cb4f96c7944f22dfd3
oscar <oscarbarragan31@gmail.com>
2021-01-29 14:17:16 +0000

    0497.04 añadi dos metodos para que te mostrata y devolviera las temperaturas y las veces que han cambiado

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,9 +1,13 @@
-public class AireAcondicionado
-{
+public class AireAcondicionado{
     private double temperatura;
     private double min;
     private double max;
     private double incremento;
+    private int contadorTemp1;
+    private int contadorTemp2;
+    private double temperaturaSub;
+    private double temperaturaBaj;
+    
 
     public AireAcondicionado(double temperaturaMin, double temperaturaMax)
     {
@@ -11,17 +15,29 @@ public class AireAcondicionado
         min = temperaturaMin;
         max = temperaturaMax;
         incremento = 5.0;
+        contadorTemp1 = 0;
+        contadorTemp2 = 0;
+        temperaturaSub = temperatura;
+        temperaturaBaj = temperatura;
     }
   
     public void subirTemperatura(){
-        if( temperatura + incremento <= max){
+        if(temperatura + incremento <= max){
             temperatura = temperatura + incremento;
+            contadorTemp1 = contadorTemp1 + 1;
+            if(temperatura > temperaturaSub){
+                temperaturaSub = temperatura;
+            }
         }
     }
  
     public void bajarTemperatura(){
         if(temperatura - incremento >= min){
-            temperatura = temperatura - incremento;
+            temperatura= temperatura - incremento;
+            contadorTemp2 = contadorTemp2 + 1;
+            if(temperatura < temperaturaBaj){
+                temperaturaBaj = temperatura;
+            }
         }
     }
     
@@ -33,5 +49,21 @@ public class AireAcondicionado
         if (incremento >= 0){
             this.incremento = incremento;
         }
+    }
+    
+    public void mostrarEstadisticas(){
+        int tempCambiada = contadorTemp1 + contadorTemp2;
+        
+        System.out.println(" Temperatura actual: " + temperatura + " - Temp. maxima posible: " + max + 
+                           " - Temp. minima posible: " + min + " - Temp. minima seleccionada: " +  temperaturaBaj +
+                           " - Temp. max seleccionada: " + temperaturaSub + " - Temperatura cambiada " + tempCambiada + " veces");
+    }
+    
+    public String getDetalles(){
+        int tempCambiada = contadorTemp1 + contadorTemp2;
+        
+        return " Temperatura actual: " + temperatura + " - Temp. maxima posible: " + max + 
+                           " - Temp. minima posible: " + min + " - Temp. minima seleccionada: " +  temperaturaBaj +
+                           " - Temp. max seleccionada: " + temperaturaSub + " - Temperatura cambiada " + tempCambiada + " veces";
     }
 }
